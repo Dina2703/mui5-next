b4ed71fa390c5d7494e2ed1275bc7dfd3c2e6a2b
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-04 10:49:11 -0500

    Add A Logo (Brand)

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,9 @@ import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
 import { styled } from "@mui/system";
+import { IconButton } from "@mui/material";
+import { Home } from "@mui/icons-material";
+import MuiNextLink from "./MuiNextLink";
 
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
@@ -14,7 +17,16 @@ const Header = () => {
             maxWidth="lg"
             sx={{ display: `flex`, justifyContent: `space-between` }}
           >
-            This is a header
+            <IconButton edge="start" aria-label="home">
+              <MuiNextLink activeClassName="active" href="/">
+                <Home
+                  sx={{
+                    color: (theme) => theme.palette.common.white,
+                  }}
+                  fontSize="large"
+                />
+              </MuiNextLink>
+            </IconButton>
           </Container>
         </Toolbar>
       </AppBar>
