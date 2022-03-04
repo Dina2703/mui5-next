f0879c3a9ea9feec6b45eaeacda0f62809f1e245
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-04 11:07:53 -0500

    The Side Drawer(hamburger) & Hide on Scroll

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,6 +6,8 @@ import { IconButton } from "@mui/material";
 import { Home } from "@mui/icons-material";
 import MuiNextLink from "./MuiNextLink";
 import Navbar from "./Navbar";
+import SideDrawer from "./SideDrawer";
+import HideOnScroll from "./HideOnScroll";
 
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
@@ -20,26 +22,29 @@ export const navLinks = [
 const Header = () => {
   return (
     <>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Container
-            maxWidth="lg"
-            sx={{ display: `flex`, justifyContent: `space-between` }}
-          >
-            <IconButton edge="start" aria-label="home">
-              <MuiNextLink activeClassName="active" href="/">
-                <Home
-                  sx={{
-                    color: (theme) => theme.palette.common.white,
-                  }}
-                  fontSize="large"
-                />
-              </MuiNextLink>
-            </IconButton>
-            <Navbar navLinks={navLinks} />
-          </Container>
-        </Toolbar>
-      </AppBar>
+      <HideOnScroll>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Container
+              maxWidth="lg"
+              sx={{ display: `flex`, justifyContent: `space-between` }}
+            >
+              <IconButton edge="start" aria-label="home">
+                <MuiNextLink activeClassName="active" href="/">
+                  <Home
+                    sx={{
+                      color: (theme) => theme.palette.common.white,
+                    }}
+                    fontSize="large"
+                  />
+                </MuiNextLink>
+              </IconButton>
+              <Navbar navLinks={navLinks} />
+              <SideDrawer navLinks={navLinks} />
+            </Container>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
       <Offset />
     </>
   );
